6f9be8de82bead6cfef806d430c2c032faf7e277
SStarbuckS <22103918+SStarbuckS@users.noreply.github.com>
2025-05-08 12:03:18 +0800

    迁移到 Manifest V3
    迁移到 Manifest V3

--- background.js
+++ background.js
@@ -1,228 +1,215 @@
-
-var denodify = function (fn) {
+const denodify = (fn) => {
 	return function () {
-		var args = Array.prototype.slice.call(arguments);
-		return new Promise(function (fulfill, reject) {
+		const args = Array.prototype.slice.call(arguments);
+		return new Promise((fulfill, reject) => {
 			fn.apply(null, args.concat([fulfill]));
 		});
 	};
 };
 
-var sendMessageToCurrentTab = function (message) {
-	return new Promise(function (fulfill, reject) {
-		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-			chrome.tabs.sendMessage(tabs[0].id, message, {}, fulfill);
-		});
-	});
+const sendMessageToCurrentTab = async (message) => {
+	const [tab] = await chrome.tabs.query({active: true, currentWindow: true});
+	return chrome.tabs.sendMessage(tab.id, message);
 };
 
-var resetLocalStorageByDomain = function (domain, content) {
+const resetLocalStorageByDomain = async (domain, content) => {
 	return sendMessageToCurrentTab({
 		op: 'resetLocalStorageByDomain',
 		params: {domain: domain, content: content},
 	});
 };
 
-var getLocalStorageByDomain = function (domain) {
+const getLocalStorageByDomain = async (domain) => {
 	return sendMessageToCurrentTab({
 		op: 'getLocalStorageByDomain',
 		params: {domain: domain},
 	});
 };
 
-var hostToDomain = function (host) {
-	var a = host.split('.');
-	if (a.length > 2)
+const hostToDomain = (host) => {
+	let a = host.split('.');
+	if (a.length > 2) {
 		a = a.slice(a.length-2);
+	}
 	return a.join('.');
 };
 
-var cookieUrl = function (c) {
+const cookieUrl = (c) => {
 	return (c.secure?'https':'http')+'://'+c.domain.replace(/^\./, '');
 };
 
 // cookies={cookies,localStorage}
 
-var getAllCookiesByDomain = function (domain) {
-	var res = {}
-	return denodify(chrome.cookies.getAll)({domain: domain}).then(function (cookies) {
-		res.cookies = cookies;
-		return getLocalStorageByDomain(domain);
-	}).then(function (content) {
-		res.localStorage = content;
-		return res;
-	});
+const getAllCookiesByDomain = async (domain) => {
+	const res = {};
+	const cookies = await chrome.cookies.getAll({domain: domain});
+	res.cookies = cookies;
+	res.localStorage = await getLocalStorageByDomain(domain);
+	return res;
 };
 
-var removeAllCookiesByDomain  = function (domain) {
-	return getAllCookiesByDomain(domain).then(function (cookies) {
-		return Promise.all([
-			removeAllChromeCookies(cookies.cookies),
-			resetLocalStorageByDomain(domain, {}),
-		]);
-	});
+const removeAllCookiesByDomain = async (domain) => {
+	const cookies = await getAllCookiesByDomain(domain);
+	await Promise.all([
+		removeAllChromeCookies(cookies.cookies),
+		resetLocalStorageByDomain(domain, {}),
+	]);
 };
 
-var resetAllCookiesByDomain = function (domain, cookies) {
-	return removeAllCookiesByDomain(domain).then(function () {
-		return Promise.all([
-			setAllChromeCookies(cookies.cookies),
-			resetLocalStorageByDomain(domain, cookies.localStorage),
-		])
-	});
+const resetAllCookiesByDomain = async (domain, cookies) => {
+	await removeAllCookiesByDomain(domain);
+	await Promise.all([
+		setAllChromeCookies(cookies.cookies),
+		resetLocalStorageByDomain(domain, cookies.localStorage),
+	]);
 };
 
-var removeAllChromeCookies = function (cookies) {
-	return Promise.all(cookies.map(function (c) {
-		return denodify(chrome.cookies.remove)({
+const removeAllChromeCookies = async (cookies) => {
+	await Promise.all(cookies.map(c => 
+		chrome.cookies.remove({
 			url: cookieUrl(c),
 			name: c.name,
 			storeId: c.storeId,
-		});
-	}));
+		})
+	));
 };
 
-var setAllChromeCookies = function (cookies) {
-	return Promise.all(cookies.map(function (c) {
-		var set = {
+const setAllChromeCookies = async (cookies) => {
+	await Promise.all(cookies.map(c => {
+		const set = {
 			url: cookieUrl(c),
-			name: c.name, value: c.value,
-			domain: c.domain, path: c.path,
-			secure: c.secure, httpOnly: c.httpOnly,
-			expirationDate: c.expirationDate, storeId: c.storeId,
+			name: c.name,
+			value: c.value,
+			domain: c.domain,
+			path: c.path,
+			secure: c.secure,
+			httpOnly: c.httpOnly,
+			expirationDate: c.expirationDate,
+			storeId: c.storeId,
 		};
-		return denodify(chrome.cookies.set)(set);
+		return chrome.cookies.set(set);
 	}));
 };
 
-var storageGet = function (k, v) {
-	var storage = chrome.storage.local;
-	return denodify(storage.get.bind(storage))(k).then(function (res) {
-		return res[k] || v;
-	});
+const storageGet = async (k, v) => {
+	const result = await chrome.storage.local.get(k);
+	return result[k] || v;
 };
 
-var storageSet = function (k, v) {
-	var storage = chrome.storage.local;
-	var p = {};
+const storageSet = async (k, v) => {
+	const p = {};
 	p[k] = v;
-	return denodify(storage.set.bind(storage))(p);
+	await chrome.storage.local.set(p);
 };
 
-var defaultDomainData = function () {
+const defaultDomainData = () => {
 	return {profiles: {1: {title: '默认'}}, currentProfileId: 1};
 };
 
-var api = {};
-
-api.deleteCurrentProfile = function (params) {
-	var domain = hostToDomain(params.host);
-	return storageGet(domain).then(function (data) {
-		if (data == null)
-			return;
-		if (Object.keys(data.profiles).length <= 1)
-			return;
+// API 实现
+const api = {
+	async deleteCurrentProfile(params) {
+		const domain = hostToDomain(params.host);
+		const data = await storageGet(domain);
+		if (!data || Object.keys(data.profiles).length <= 1) return;
+		
 		delete data.profiles[data.currentProfileId];
 		data.currentProfileId = Object.keys(data.profiles)[0];
-		return storageSet(domain, data).then(function () {
-			return resetAllCookiesByDomain(domain, data.profiles[data.currentProfileId].cookies);
-		});
-	});
-};
+		await storageSet(domain, data);
+		return resetAllCookiesByDomain(domain, data.profiles[data.currentProfileId].cookies);
+	},
 
-api.newProfile = function (params) {
-	var domain = hostToDomain(params.host);
-	return storageGet(domain).then(function (data) {
-		if (data == null)
-			data = defaultDomainData();
+	async newProfile(params) {
+		const domain = hostToDomain(params.host);
+		let data = await storageGet(domain);
+		if (!data) data = defaultDomainData();
 
-		var oldProfile = data.profiles[data.currentProfileId];
-		return getAllCookiesByDomain(domain).then(function (cookies) {
-			oldProfile.cookies = cookies;
+		const oldProfile = data.profiles[data.currentProfileId];
+		const cookies = await getAllCookiesByDomain(domain);
+		oldProfile.cookies = cookies;
 
-			var newProfile = {title: '马甲'+Object.keys(data.profiles).length};
-			var newProfileId = Date.now();
-			data.profiles[newProfileId] = newProfile;
-			data.currentProfileId = newProfileId;
-		}).then(function () {
-			return removeAllCookiesByDomain(domain);
-		}).then(function () {
-			return storageSet(domain, data);
-		});
-	});
-};
+		const newProfile = {title: '马甲'+Object.keys(data.profiles).length};
+		const newProfileId = Date.now();
+		data.profiles[newProfileId] = newProfile;
+		data.currentProfileId = newProfileId;
 
-api.updateProfile = function (params) {
-	var domain = hostToDomain(params.host);
-	return storageGet(domain).then(function (data) {
-		if (data == null)
-			return;
-		var id = params.id || data.currentProfileId;
-		var profile = data.profiles[id];
-		if (profile == null)
-			return;
-		for (var k in params.$set || {}) {
-			var v = params.$set[k];
-			profile[k] = v;
+		await removeAllCookiesByDomain(domain);
+		await storageSet(domain, data);
+	},
+
+	async updateProfile(params) {
+		const domain = hostToDomain(params.host);
+		const data = await storageGet(domain);
+		if (!data) return;
+
+		const id = params.id || data.currentProfileId;
+		const profile = data.profiles[id];
+		if (!profile) return;
+
+		if (params.$set) {
+			Object.assign(profile, params.$set);
 		}
+		await storageSet(domain, data);
+	},
 
-		return storageSet(domain, data);
-	});
-};
+	async selectProfile(params) {
+		const domain = hostToDomain(params.host);
+		const data = await storageGet(domain);
+		if (!data || params.id === data.currentProfileId) return;
 
-api.selectProfile = function (params) {
-	var domain = hostToDomain(params.host);
-	return storageGet(domain).then(function (data) {
-		if (data == null)
-			return;
-		if (params.id == data.currentProfileId)
-			return;
+		const oldProfile = data.profiles[data.currentProfileId];
+		const newProfile = data.profiles[params.id];
+		if (!oldProfile || !newProfile) return;
 
-		var oldProfile = data.profiles[data.currentProfileId];
-		var newProfile = data.profiles[params.id];
-		if (oldProfile == null || newProfile == null)
-			return;
+		const cookies = await getAllCookiesByDomain(domain);
+		oldProfile.cookies = cookies;
+		data.currentProfileId = params.id;
+		await storageSet(domain, data);
+		await resetAllCookiesByDomain(domain, newProfile.cookies);
+	},
 
-		return getAllCookiesByDomain(domain).then(function (cookies) {
-			oldProfile.cookies = cookies;
-			data.currentProfileId = params.id;
-			return storageSet(domain, data);
-		}).then(function () {
-			return resetAllCookiesByDomain(domain, newProfile.cookies);
-		});
-	});
-};
+	async getProfiles(params) {
+		const domain = hostToDomain(params.host);
+		const data = await storageGet(domain);
+		return data || defaultDomainData();
+	},
 
-api.getProfiles = function (params) {
-	var domain = hostToDomain(params.host);
-	return storageGet(domain).then(function (data) {
-		if (data == null)
-			return defaultDomainData();
-		return data;
-	});
-};
-
-api.updateUserInfo = function (params) {
-	var domain = hostToDomain(params.host);
-	return storageGet(domain).then(function (data) {
-		if (data == null)
-			data = defaultDomainData();
-		var profile = data.profiles[data.currentProfileId];
+	async updateUserInfo(params) {
+		const domain = hostToDomain(params.host);
+		let data = await storageGet(domain);
+		if (!data) data = defaultDomainData();
+		
+		const profile = data.profiles[data.currentProfileId];
 		profile.title = params.username;
-		return storageSet(domain, data);
-	});
+		await storageSet(domain, data);
+	}
 };
 
-chrome.runtime.onMessage.addListener(function (msg, sender, cb) {
-	var fulfill = function (res) {
-		console.log(msg, res);
-		cb(res);
-	};
-
-	var func = api[msg.op];
-	if (func)
-		func(msg.params).then(fulfill);
-
-	return true;
+// 消息监听器
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+	if (api[message.op]) {
+		api[message.op](message.params)
+			.then(sendResponse)
+			.catch(error => sendResponse({ error: error.message }));
+		return true; // 保持消息通道开放
+	}
 });
+
+// 导出需要的函数
+export {
+	denodify,
+	sendMessageToCurrentTab,
+	resetLocalStorageByDomain,
+	getLocalStorageByDomain,
+	hostToDomain,
+	cookieUrl,
+	getAllCookiesByDomain,
+	removeAllCookiesByDomain,
+	resetAllCookiesByDomain,
+	removeAllChromeCookies,
+	setAllChromeCookies,
+	storageGet,
+	storageSet,
+	defaultDomainData
+};
 
--- contentscript.js
+++ contentscript.js
@@ -1,89 +1,94 @@
+// 使用 ES 模块格式
+const callApi = async (op, params = {}) => {
+	params.host = window.location.hostname;
+	return chrome.runtime.sendMessage({ op, params });
+};
 
-!function () {
-	var callApi = function (op, params) {
-		return new Promise(function (fulfill, reject) {
-			params = params || {};
-			params.host = window.location.hostname;
-			chrome.runtime.sendMessage(null, {op: op, params: params}, {}, fulfill);
-		});
-	};
+const resetLocalStorage = (content) => {
+	sessionStorage.clear();
+	localStorage.clear();
+	for (const k in content) {
+		localStorage.setItem(k, content[k]);
+	}
+};
 
-	chrome.runtime.onMessage.addListener(function (msg, sender, cb) {
-		if (msg.op == 'resetLocalStorageByDomain') {
-			if (msg.params.domain == domain) {
-				resetLocalStorage(msg.params.content);
-				cb(true);
-			}
-		} else if (msg.op == 'getLocalStorageByDomain') {
-			if (msg.params.domain == domain) {
-				var res = {}
-				for (var k in localStorage)
-					res[k] = localStorage.getItem(k);
-				cb(res);
-			}
+const gotUsername = async (name) => {
+	if (localStorage.getItem('majia.username') === name) return;
+	localStorage.setItem('majia.username', name);
+	await callApi('updateUserInfo', { username: name });
+};
+
+const hostToDomain = (host) => {
+	let a = host.split('.');
+	if (a.length > 2) {
+		a = a.slice(a.length - 2);
+	}
+	return a.join('.');
+};
+
+const domain = hostToDomain(window.location.hostname);
+
+// 消息监听器
+chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+	if (msg.op === 'resetLocalStorageByDomain') {
+		if (msg.params.domain === domain) {
+			resetLocalStorage(msg.params.content);
+			sendResponse(true);
 		}
-		return true;
-	});
-
-	var resetLocalStorage = function (content) {
-		sessionStorage.clear();
-		localStorage.clear();
-		for (var k in content)
-			localStorage.setItem(k, content[k]);
-	};
-
-	var gotUsername = function (name) {
-		if (localStorage.getItem('majia.username') == name)
-			return;
-		localStorage.setItem('majia.username', name);
-		callApi('updateUserInfo', {username: name});
-	};
-
-	var hostToDomain = function (host) {
-		var a = host.split('.');
-		if (a.length > 2)
-			a = a.slice(a.length-2);
-		return a.join('.');
-	};
-	var domain = hostToDomain(window.location.hostname);
-
-	({
-		'douban.com': function () {
-			var span = document.querySelector('.nav-user-account span');
-			if (span) {
-				var m = span.innerHTML.match(new RegExp('(.*)的帐号'));
-				m && m[1] && gotUsername(m[1]);
+	} else if (msg.op === 'getLocalStorageByDomain') {
+		if (msg.params.domain === domain) {
+			const res = {};
+			for (const k in localStorage) {
+				res[k] = localStorage.getItem(k);
 			}
-		},
+			sendResponse(res);
+		}
+	}
+	return true;
+});
 
-		'zhihu.com': function () {
-			var span = document.querySelector('.top-nav-profile .name');
-			if (span && span.innerHTML)
-				gotUsername(span.innerHTML);
-		},
-
-		'weibo.com': function () {
-			var em = document.querySelector('[nm="name"] .S_txt1');
-			if (em && em.innerHTML) {
-				gotUsername(em.innerHTML);
-				return;
-			}
-			var m = document.cookie.match(/un=([^;]+);/);
+// 网站特定的用户名检测
+const siteHandlers = {
+	'douban.com': () => {
+		const span = document.querySelector('.nav-user-account span');
+		if (span) {
+			const m = span.innerHTML.match(/(.*)的帐号/);
 			m && m[1] && gotUsername(m[1]);
-		},
+		}
+	},
 
-		'twitter.com': function () {
-			var span = document.querySelector('.DashboardProfileCard-screennameLink span');
-			if (span && span.innerHTML)
-				gotUsername(span.innerHTML);
-		},
+	'zhihu.com': () => {
+		const span = document.querySelector('.top-nav-profile .name');
+		if (span && span.innerHTML) {
+			gotUsername(span.innerHTML);
+		}
+	},
 
-		'facebook.com': function () {
-			var span = document.querySelector('[data-click="profile_icon"] span');
-			if (span && span.innerHTML)
-				gotUsername(span.innerHTML);
-		},
+	'weibo.com': () => {
+		const em = document.querySelector('[nm="name"] .S_txt1');
+		if (em && em.innerHTML) {
+			gotUsername(em.innerHTML);
+			return;
+		}
+		const m = document.cookie.match(/un=([^;]+);/);
+		m && m[1] && gotUsername(m[1]);
+	},
 
-	}[domain] || function () {})();
-}();
+	'twitter.com': () => {
+		const span = document.querySelector('.DashboardProfileCard-screennameLink span');
+		if (span && span.innerHTML) {
+			gotUsername(span.innerHTML);
+		}
+	},
+
+	'facebook.com': () => {
+		const span = document.querySelector('[data-click="profile_icon"] span');
+		if (span && span.innerHTML) {
+			gotUsername(span.innerHTML);
+		}
+	}
+};
+
+// 执行对应网站的处理器
+(siteHandlers[domain] || (() => {}))();
 
